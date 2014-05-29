2dd07bde2ecaeb0fb68a3c51860a65c71ce1c511
shimon caplan <shimcaps@gmail.com>
2014-05-28 21:28:37 -0400

    homework 5

---
+++ second.js
@@ -0,0 +1,41 @@
+
+function tellFurtune(job, city, partner, children) 
+
+{
+ var future = ("you will be a " + job + " in " + city + " and married  to " + partner +
+ 	" with " + children + " children.")
+ console.log (future)
+}
+
+
+tellFurtune ( "social worker", "Colorodo", "mashi", 5)
+
+
+tellFurtune ( "librarian", "atlanta", "Greg", 3)
+
+tellFurtune ( "comedian", "africe", "haiuohfgi", 90)
+
+
+
+var age = 2014
+
+var dob = 1987
+
+var bage = 26
+
+var page = 27
+
+console.log ("you are either " + bage + " or " + page + " years old.");
+
+
+
+function calculateAge(birthyear, currentyear)
+{
+     var age = birthyear - currentyear;
+     console.log (" you are either " + age + " or "  + (age - 1));
+}
+
+calculateAge (1987, 2014);
+calculateAge (1888, 2014);
+
+
